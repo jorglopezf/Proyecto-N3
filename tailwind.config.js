0b607d54ff6f18af3e9daae7e582307c7f8e07c3
jorglopezf <jorg.lopefigueroa@gmail.com>
2024-04-24 12:09:49 -0400

    v3.
    falta agregar pag de los  servicios y nosotros

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    //clases personalizadas
+
     extend: {
-      
+      fontFamily:{
+        'kminfo': ['Anton', 'sans-serif'],
+        'nav-fo': ['Mate SC', 'sans-serif'],
+        'parra': ['Signika Negative', 'sans-serif'],
+      },
+      width: {
+        'caja-ancho': '240px',
+        '100%': '100%',
+      },
+      height: {
+        'caja-alto': '300px',
+        '100vh': '100vh',
+      },
+      gridTemplateColumns: {
+        '3columnas': 'repeat(3, minmax(0, 1fr))',
+      },
     },
   },
   plugins: [],
